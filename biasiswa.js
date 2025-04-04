cbaa2635a9a07bd080d67be2622b6a3936b11217
cikguainul <ainul.wec@gmail.com>
2025-04-04 15:47:38 +0800

    Delete biasiswa.js

--- biasiswa.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  const biasiswa = [
-    { nama: "Biasiswa Yayasan Khazanah", pautan: "https://www.yayasankhazanah.com.my" },
-    { nama: "Biasiswa MARA", pautan: "https://www.mara.gov.my" },
-    { nama: "YTP – Yayasan Telekom", pautan: "https://ytp.tm.com.my" },
-  ];
-  res.status(200).json({ biasiswa });
-}
